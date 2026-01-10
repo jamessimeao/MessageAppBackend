13b42aa28f5bc69c5ea16580c837987bc8f578ca
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 19:42:26 -0300

    Create IAuthService
    Interface for authorization service. It has methods to register a new user, login and refresh the JWT. If the refresh token is valid, it issues a new access token.

---
+++ JWTAuth/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using JWTAuth.Dtos;
+using JWTAuth.Models;
+
+namespace JWTAuth.Services
+{
+    public interface IAuthService
+    {
+        public Task<User?> RegisterAsync(UserDto userDto);
+        public Task<TokenDto?> LoginAsync(UserDto userDto);
+        public Task<TokenDto?> RefreshTokenAsync(TokenDto oldTokenDto);
+    }
+}
