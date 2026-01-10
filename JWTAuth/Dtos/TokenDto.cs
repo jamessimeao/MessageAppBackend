672acda124d4f51212af62add374740f0ba09571
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-10 19:25:26 -0300

    Create a dto for the JWT

---
+++ JWTAuth/Dtos/TokenDto.cs
@@ -0,0 +1,16 @@
+﻿namespace JWTAuth.Dtos
+{
+    public class TokenDto
+    {
+        // The access token is used to identify the user.
+        // It is short lived, for this reason we have also a refresh token,
+        // to request for new access tokens without having to login.
+        public required string AccessToken { get; set; }
+
+        // The refresh token is used to request a new access token once it has expired.
+        // The refresh token is valid for more time than the access token.
+        // Once the refresh token is invalid, the user will have to login again to recreate
+        // both the access token and refresh token.
+        public required string RefreshToken { get; set; }
+    }
+}
