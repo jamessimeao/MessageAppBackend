54f38b2578ceb7ef96279960ee1cc44b88c0e02c
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-14 23:19:27 -0300

    Make a stored procedure to get a user from its email.
    Each email should be used by a single user.

---
+++ JWTAuth/Data/SQL/stored procedures/getUserFromEmail.sql
@@ -0,0 +1,24 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		James
+-- Create date: 13/01/2026
+-- Description: Get password hash of user with given email
+-- =============================================
+CREATE PROCEDURE dbo.getUserFromEmail
+	-- Add the parameters for the stored procedure here
+	@email NVARCHAR(320) = NULL
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT id, email, passwordhash, username, userrole
+		FROM dbo.users
+		WHERE email = @email;
+END
+GO
