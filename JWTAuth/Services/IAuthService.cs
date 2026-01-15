df06eaff63c765ae6555b1d2821d43c5d93b7c25
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-14 23:29:10 -0300

    Change method name from RefreshTokenAsync to RefreshAccessTokenAsync
    So it is clear that only the access token will be refreshed. The refresh token will stay the same. A new refresh token can be obtained if the user login again.

--- JWTAuth/Services/IAuthService.cs
+++ JWTAuth/Services/IAuthService.cs
@@ -6,8 +6,8 @@ namespace JWTAuth.Services
 {
     public interface IAuthService
     {
-        public Task<TokenDto?> RefreshTokenAsync(int userId, string refreshToken);
         public Task<User?> RegisterAsync(UserRegisterDto userRegisterDto);
         public Task<TokenDto?> LoginAsync(UserLoginDto userLoginDto);
+        public Task<TokenDto?> RefreshAccessTokenAsync(int userId, string refreshToken);
     }
 }
