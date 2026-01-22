cad36d8a590d9ba34102ee1f225eff9e903be769
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-22 20:43:53 -0300

    Create class for units tests for AuthService

---
+++ JWTAuthTests/UnitTests/AuthServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using JWTAuth.Data;
+using JWTAuth.Dtos;
+using JWTAuth.Models;
+using JWTAuth.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace JWTAuthTests.UnitTests
+{
+    public class AuthServiceTests
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public AuthServiceTests()
+        {
+            // Configuration
+            Dictionary<string, string?> configurationDict = new()
+            {
+                { "AppSettings:Token", "abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789"},
+                { "AppSettings:Issuer", "issuer"},
+                { "AppSettings:Audience", "audience"},
+            };
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            _configuration = configurationBuilder.AddInMemoryCollection(configurationDict).Build();
+        }
+
+    }
+}
