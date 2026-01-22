fe36dcf75d69fe1d946644554d0addc530f1f2a1
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-01-22 20:44:28 -0300

    Create unit test RegisterAsync_ReturnsUser_ForNewUser

--- JWTAuthTests/UnitTests/AuthServiceTests.cs
+++ JWTAuthTests/UnitTests/AuthServiceTests.cs
@@ -25,5 +25,40 @@ namespace JWTAuthTests.UnitTests
             _configuration = configurationBuilder.AddInMemoryCollection(configurationDict).Build();
         }
 
+        [Fact]
+        public async Task RegisterAsync_ReturnsUser_ForNewUser()
+        {
+            //********************* Arrange *************************
+            UserRegisterDto userRegisterDto = new()
+            {
+                Email = "name@company.com",
+                Password = "password",
+                Username = "name",
+            };
+
+            PasswordHasher<User> passwordHasher = new();
+
+            // Mock the IDataAccess
+            Mock<IDataAccess> dataAccessMock = new();
+            dataAccessMock.Setup(dataAccess => dataAccess.UserExistsAsync(userRegisterDto)).Returns(Task.FromResult(false));
+
+            // Create the AuthService
+            AuthService authService = new AuthService(_configuration, dataAccessMock.Object);
+
+            //********************* Act *************************
+            User? user = await authService.RegisterAsync(userRegisterDto);
+            PasswordVerificationResult? result = null;
+            if(user != null)
+            {
+                result = passwordHasher.VerifyHashedPassword(user, user.PasswordHash, userRegisterDto.Password);
+            }
+
+            //********************* Assert *************************
+            Assert.NotNull(user);
+            Assert.NotNull(result);
+            Assert.Equal(userRegisterDto.Email, user.Email);
+            Assert.Equal(userRegisterDto.Username, user.Username);
+            Assert.Equal(PasswordVerificationResult.Success, result);
+        }
     }
 }
