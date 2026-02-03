903f209e1770cddc6afbeb0d93b3385d1479cb15
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 11:20:52 -0300

    Configure SignalR in Program.cs

--- Message/Program.cs
+++ Message/Program.cs
@@ -1,3 +1,8 @@
+using Message.Hubs;
+using Message.UserIdProvider;
+using Microsoft.AspNetCore.SignalR;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSignalR();
+// To uniquely identify the user of SignalR
+builder.Services.AddSingleton<IUserIdProvider, UserIdProvider>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,5 +26,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapHub<ChatHub>("/Message");
 
 app.Run();
