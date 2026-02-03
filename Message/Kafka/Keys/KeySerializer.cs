91b116f4a3a06b6c731bf7f484ba13a49ef765f1
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 11:03:53 -0300

    Make a KeySerializer to serialize the Kafka key to bytes

---
+++ Message/Kafka/Keys/KeySerializer.cs
@@ -0,0 +1,16 @@
+﻿using Confluent.Kafka;
+using System.Text;
+using System.Text.Json;
+
+namespace Message.Kafka.Keys
+{
+    public class KeySerializer : ISerializer<Key>
+    {
+        public byte[] Serialize(Key data, SerializationContext context)
+        {
+            string serializedToString = JsonSerializer.Serialize(data);
+            byte[] bytes = Encoding.UTF8.GetBytes(serializedToString);
+            return bytes;
+        }
+    }
+}
