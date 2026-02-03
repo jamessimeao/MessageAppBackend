2b03e8e0a5f9aa49a9d6720e4ec405e36666e954
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 11:03:27 -0300

    Make a key to be used by Kafka

---
+++ Message/Kafka/Keys/Key.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Message.Kafka.Keys
+{
+    public class Key
+    {
+        public required string SenderId { get; set; }
+        public required string ReceiverId { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
