5b540d9270ab54d86f9245a12bf429ff7178a049
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 11:08:22 -0300

    Make an interface IChatClient that abstracts the methods of the SignalR client
    This is used for strongly typing the Hub in the server, which prevents errors.

---
+++ Message/Hubs/IChatClient.cs
@@ -0,0 +1,12 @@
+﻿namespace Message.Hubs
+{
+    // Interface that abstracts the Hub from the client.
+    // It is used by SignalR for strongly typing.
+    // Instead of passing a string with the name of a client's method,
+    // we use a method from this interface.
+    public interface IChatClient
+    {
+        public Task ReceiveMessageAsync(string senderId, string message);
+        public Task ReceiveErrorMessageAsync(string errorMessage);
+    }
+}
