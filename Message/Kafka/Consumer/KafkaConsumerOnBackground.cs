97f18e7c9bee7866a947a9689a2bd6e337001fb7
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 11:30:02 -0300

    Make a background service to run a Kafka consumer
    The Kafka consumer should be running continuously. It blocks the thread, so it needs to be running in the background.

---
+++ Message/Kafka/Consumer/KafkaConsumerOnBackground.cs
@@ -0,0 +1,20 @@
+﻿using Message.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Message.Kafka.Consumer
+{
+    public class KafkaConsumerOnBackground : BackgroundService
+    {
+        private readonly IKafkaConsumer _kafkaConsumer;
+
+        public KafkaConsumerOnBackground(IKafkaConsumer kafkaConsumer)
+        {
+            _kafkaConsumer = kafkaConsumer;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _kafkaConsumer.ConsumeMessagesFromKafkaAsync(stoppingToken);
+        }
+    }
+}
