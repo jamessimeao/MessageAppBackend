aac252d8c97f57b754c07dcf4a95daa675febaa1
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-03 11:17:47 -0300

    Create an user id provider to be used by ChatHub
    The ChatHub uses authorization. In this way, we can identify a user by its credentials. The user id provider uses the client's email as id.

---
+++ Message/UserIdProvider/UserIdProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+namespace Message.UserIdProvider
+{
+    public class UserIdProvider : IUserIdProvider
+    {
+        public string? GetUserId(HubConnectionContext connection)
+        {
+            // Identify the user uniquely by its email
+            return connection.User?.FindFirst(ClaimTypes.Email)?.Value;
+        }
+    }
+}
