4b4563ff1586eac18619f3e35362a12160f6042a
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-12 18:43:16 -0300

    Setup JWT authentication

--- Rooms/Program.cs
+++ Rooms/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using Rooms.Data;
 using System.Data;
 using System.Text;
 
@@ -30,6 +33,36 @@ builder.Services.AddTransient<IDbConnection>
 (
     (IServiceProvider serviceProvider) => new SqlConnection(connectionString)
 );
+
+// Dependency injection for DataAccess class
+builder.Services.AddTransient<IDataAccess, DataAccess>();
+
+string? appSettingsTokenNullable = builder.Configuration.GetValue<string>("AppSettings:Token");
+string? appSettingsIssuerNullable = builder.Configuration.GetValue<string>("AppSettings:Issuer");
+string? appSettingsAudienceNullable = builder.Configuration.GetValue<string>("AppSettings:Audience");
+if (appSettingsTokenNullable == null || appSettingsIssuerNullable == null || appSettingsAudienceNullable == null)
+{
+    throw new Exception("Failed to get AppSettings token, issuer or audience.");
+}
+string appSettingsToken = appSettingsTokenNullable;
+string appSettingsIssuer = appSettingsIssuerNullable;
+string appSettingsAudience = appSettingsAudienceNullable;
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(
+        (JwtBearerOptions options) =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidIssuer = appSettingsIssuer,
+                ValidateAudience = true,
+                ValidAudience = appSettingsAudience,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettingsToken)),
+                ValidateLifetime = true
+            };
+        }
+    );
 
 var app = builder.Build();
 
