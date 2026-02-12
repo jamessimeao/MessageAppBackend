86cc75565d456851d3f1b1cfad0538eb84b8bd20
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-11 22:29:05 -0300

    Configure database connection
    For the moment, the database connection is provided as a singleton service. This is because the connection is only used by the consumer, which runs constantly on the background. It may be replaced by a factory of connections, so the consumer only connects to the database when needed.

--- KafkaConsumer/Program.cs
+++ KafkaConsumer/Program.cs
@@ -1,4 +1,7 @@
+using KafkaConsumer.Data;
 using KafkaConsumer.Kafka;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +10,28 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
+
+if (builder.Environment.IsProduction())
+{
+    // For K8s deployment.
+    // Some secrets will be created in Kubernetes
+    builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("secrets/db-settings.json");
+}
+
+// Dependency injection for database connection, to be used by DataAccess class
+string? connectionString = builder.Configuration.GetConnectionString("Default");
+if (connectionString == null)
+{
+    throw new Exception("Failed to get connection string.");
+}
+
+builder.Services.AddSingleton<IDbConnection>
+(
+    (IServiceProvider serviceProvider) => new SqlConnection(connectionString)
+);
+
+// Dependency injection for DataAccess class
+builder.Services.AddSingleton<IDataAccess, DataAccess>();
 
 // Kafka consumer
 // Won't use dependency injection for the consumer.
