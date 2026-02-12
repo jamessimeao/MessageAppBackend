71c85ffddd965d1559df6173f2e519c133259f87
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-12 18:44:13 -0300

    Make a stored procedure to delete a room

---
+++ SQL/stored procedures/rooms table/deleteRoom.sql
@@ -0,0 +1,23 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		James
+-- Create date: 12/02/2026
+-- Description:	delete room with given id
+-- =============================================
+CREATE PROCEDURE dbo.deleteRoom
+	-- Add the parameters for the stored procedure here
+	@id INT = NULL
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	DELETE FROM dbo.rooms
+		WHERE id = @id;
+END
+GO
