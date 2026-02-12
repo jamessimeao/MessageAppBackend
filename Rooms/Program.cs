cb8e97819d1cac0c7c27be47f8cbe3aa9f3e32b4
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-12 18:42:45 -0300

    Configure database connection

--- Rooms/Program.cs
+++ Rooms/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +10,26 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+if (builder.Environment.IsProduction())
+{
+    // For K8s deployment.
+    // Some secrets will be created in Kubernetes
+    // which will be copied the json files secrets/jwt-settings.json and secrets/db-settings.json.
+    builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("secrets/jwt-settings.json");
+    builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("secrets/db-settings.json");
+}
+
+// Dependency injection for database connection, to be used by DataAccess class
+string? connectionString = builder.Configuration.GetConnectionString("Default");
+if (connectionString == null)
+{
+    throw new Exception("Failed to get connection string.");
+}
+
+builder.Services.AddTransient<IDbConnection>
+(
+    (IServiceProvider serviceProvider) => new SqlConnection(connectionString)
+);
 
 var app = builder.Build();
 
