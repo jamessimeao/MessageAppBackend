982dda01b8aebb83457876ee04097ad3f96aa03a
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-12 21:30:43 -0300

    Correct procedure name

--- Message/Data/DataAccess.cs
+++ Message/Data/DataAccess.cs
@@ -6,6 +6,9 @@ namespace Message.Data
     public class DataAccess(IDbConnection connection) : IDataAccess
     {
         private const string USERID_VARIABLE = "userid";
+
+        private const string GET_USER_ROOMS_PROCEDURE = "dbo.getUserRooms";
+
         public async Task<IEnumerable<int>> GetRoomIdsAsync(int userId)
         {
             DynamicParameters parameters = new DynamicParameters();
@@ -13,7 +16,7 @@ namespace Message.Data
 
             IEnumerable<int> roomIds = await connection.QueryAsync<int>
             (
-                USERID_VARIABLE,
+                GET_USER_ROOMS_PROCEDURE,
                 parameters,
                 commandType: CommandType.StoredProcedure
             );
