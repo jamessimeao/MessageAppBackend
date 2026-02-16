35fab093971843485e0da1500790fc084a5517f4
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-16 17:30:31 -0300

    Create a project for the messaging API, responsible for the part that doesn't require real-time communication
    There are some functionalities that don't require real-time communication, such as loading messages from the database or deleting messages.

---
+++ MessageREST/Controllers/MessageController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MessageREST.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class MessageController : ControllerBase
+    {
+    }
+}
