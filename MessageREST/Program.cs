3d492470f214921b9c45b8d38d0ec46f580aee3a
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-16 18:39:42 -0300

    Configure database connection

--- MessageREST/Program.cs
+++ MessageREST/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.SqlClient;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,26 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+if (builder.Environment.IsProduction())
+{
+    // For K8s deployment.
+    // Some secrets will be created in Kubernetes
+    // which will be copied the json files secrets/jwt-settings.json and secrets/db-settings.json.
+    builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("secrets/jwt-settings.json");
+    builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("secrets/db-settings.json");
+}
+
+// Dependency injection for database connection, to be used by DataAccess class
+string? connectionString = builder.Configuration.GetConnectionString("Default");
+if (connectionString == null)
+{
+    throw new Exception("Failed to get connection string.");
+}
+
+builder.Services.AddTransient<IDbConnection>
+(
+    (IServiceProvider serviceProvider) => new SqlConnection(connectionString)
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
