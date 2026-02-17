b489f54be05c96da854da502a617e042b51048f8
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-17 16:27:05 -0300

    Create dto to edit a message

---
+++ MessageREST/Dtos/EditMessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MessageREST.Dtos
+{
+    public class EditMessageDto
+    {
+        public required int MessageId { get; set; }
+        public required string NewMessage { get; set; }
+    }
+}
