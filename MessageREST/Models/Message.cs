a56e2265cc541e68c9894aa22b11263737c60e65
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-17 20:28:16 -0300

    Add room id to message model

--- MessageREST/Models/Message.cs
+++ MessageREST/Models/Message.cs
@@ -3,6 +3,7 @@
     public class Message
     {
         public required int Id { get; set; }
+        public required string RoomId { get; set; }
         public required int SenderId { get; set; }
         public required string Content { get; set; }
         public DateTime Time { get; set; }
