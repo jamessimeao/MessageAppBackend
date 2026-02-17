1ec21e9e4915eef72d27025921d891ba53444f3a
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-17 16:26:39 -0300

    Create a model for the messages

---
+++ MessageREST/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageREST.Models
+{
+    public class Message
+    {
+        public required int Id { get; set; }
+        public required int SenderId { get; set; }
+        public required string Content { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
