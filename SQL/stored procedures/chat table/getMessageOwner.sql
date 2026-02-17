d5893c712e40aebd8c3cccea0ef3935ebbf5a804
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-17 16:15:29 -0300

    Create stored procedure to get the owner of a message

---
+++ SQL/stored procedures/chat table/getMessageOwner.sql
@@ -0,0 +1,24 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		James
+-- Create date: 17/02/2026
+-- Description:	get the user id of the message
+-- =============================================
+CREATE PROCEDURE dbo.getMessageOwner
+	-- Add the parameters for the stored procedure here
+	@messageid INT = NULL
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT senderid
+	FROM dbo.chat
+	WHERE id = @messageid;
+END
+GO
