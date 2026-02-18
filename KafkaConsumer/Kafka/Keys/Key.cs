e58d03ed09eb25fb1d32b22a460bf80de03e62a2
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-18 17:22:35 -0300

    Update KafkaConsumer Key

--- KafkaConsumer/Kafka/Keys/Key.cs
+++ KafkaConsumer/Kafka/Keys/Key.cs
@@ -4,11 +4,7 @@ namespace KafkaConsumer.Keys
 {
     public class Key
     {
-        public required int MessageId { get; set; }
-        public required int SenderId { get; set; }
-        public required int ReceiverId { get; set; }
-        public required DateTime Time { get; set; }
-
+        public required string EventType { get; set; }
         public override string ToString()
         {
             return JsonSerializer.Serialize(this);
