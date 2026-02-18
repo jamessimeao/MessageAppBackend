06f83c373f54a67185d46d72b5ac719e49382463
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2026-02-18 20:48:42 -0300

    Delete unused event types

--- Rooms/Kafka/EventTypes/EventType.cs
+++ Rooms/Kafka/EventTypes/EventType.cs
@@ -2,10 +2,7 @@
 {
     public enum EventType
     {
-        MESSAGE_UPDATED_EVENT,
         ROOM_CREATED_EVENT,
         ROOM_DELETED_EVENT,
-        ADD_USER_TO_ROOM_EVENT,
-        REMOVE_USER_FROM_ROOM_EVENT,
     }
 }
